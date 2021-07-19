36a777cf8d27b5714fa72f0b6813984e7ee114a0
Tim Draper <thetimdraper@gmail.com>
2021-07-19 08:57:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,70 @@
+// Imports the Google Cloud client library
 const textToSpeech = require('@google-cloud/text-to-speech');
 const fs = require('fs');
 const util = require('util');
-var player = require('play-sound')(opts = {});
 
 // Creates a client
 const client = new textToSpeech.TextToSpeechClient();
 
-var playInRealTime = true;
-
-(async function () {
+(async function() {
 
   // The text to synthesize
-  var text = fs.readFileSync('./text.txt', 'utf8');
 
-  // We split by sentences so the audio files don't break mid-sentence.
-  var arrayOfSentences = text.match(/[^\.]+\./g);
+  var text = fs.readFileSync('./text.txt', 'utf8');
+  var newArr = text.match(/[^\.]+\./g);
 
   var charCount = 0;
   var textChunk = "";
   var index = 0;
 
-  // Construct the request
-  var request = {
-    input: {
-      text: textChunk
-    },
-    voice: {
-      languageCode: 'en-US',
-      ssmlGender: 'MALE',
-      name: "en-US-Wavenet-B"
-    },
-    audioConfig: {
-      effectsProfileId: [
-        "headphone-class-device"
-      ],
-      pitch: -4,
-      speakingRate: 1.18,
-      audioEncoding: "MP3"
-    },
-  };
+  for (var n = 0; n < newArr.length; n++) {
 
-  // Iterate over sentences.
-  for (var n = 0; n < arrayOfSentences.length; n++) {
+    charCount += newArr[n].length;
+    textChunk = textChunk + newArr[n];
 
-    charCount += arrayOfSentences[n].length;
-    textChunk = textChunk + arrayOfSentences[n];
+    console.log(charCount);
 
-    // Ensure payload doesn't exceed 5k limit
-    if (charCount > 4600 || n == arrayOfSentences.length - 1) {
+    if (charCount > 4600 || n == newArr.length - 1) {
 
-      request.input.text = textChunk;
+      console.log(textChunk);
+
+      // Construct the request
+      const request = {
+        input: {
+          text: textChunk
+        },
+        // Select the language and SSML voice gender (optional)
+        voice: {
+          languageCode: 'en-US',
+          ssmlGender: 'MALE',
+          name: "en-US-Wavenet-B"
+        },
+        // select the type of audio encoding
+        audioConfig: {
+          effectsProfileId: [
+            "headphone-class-device"
+          ],
+          pitch: -2,
+          speakingRate: 1.1,
+          audioEncoding: "MP3"
+        },
+      };
 
       // Performs the text-to-speech request
       const [response] = await client.synthesizeSpeech(request);
 
+      console.log(response);
+
       // Write the binary audio content to a local file
       const writeFile = util.promisify(fs.writeFile);
-
-      var fileName = 'The_Psychology_of_Human_Misjudgment_' + index + '.mp3';
-
-      await writeFile(fileName, response.audioContent, 'binary');
+      await writeFile('Psychology_of_Human_Misjudgement_' + index + '.mp3', response.audioContent, 'binary');
       console.log('Audio content written to file: output.mp3');
+
       index++;
 
       charCount = 0;
       textChunk = "";
 
-      // Begin Playing First File
-      console.log("Playing file: " + fileName);
-
-      playInRealTime && (index == 1) && player.play('./' + fileName, function (err) {
-        console.log(err);
-      })
-
     }
   }
-}());
+}()); 
